24b39253214eea212f1bc50da35f2f565cbca0a2
jakec <jakec@DESKTOP-JTC9HU6>
2020-11-15 23:29:18 +0000

    BombSpawn done

---
+++ RWM2021-C00205926/Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour
+{
+    public GameObject bomb;
+    bool bombAlive = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump")) 
+            if(bombAlive == false)
+            {
+                {
+                    Instantiate(bomb, transform.position, Quaternion.identity);
+                    bombAlive = true;
+                }
+            }
+
+    }
+}
