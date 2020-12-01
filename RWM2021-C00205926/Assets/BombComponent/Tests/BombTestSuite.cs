edba48b251b23bb562202fba20bc85bd90eccc7b
jakec <jakec@DESKTOP-JTC9HU6>
2020-12-01 12:15:26 +0000

    Folder Fix

---
+++ RWM2021-C00205926/Assets/BombComponent/Tests/BombTestSuite.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+
+
+    public class BombTestSuite
+    {
+        // A Test behaves as an ordinary method
+        [UnityTest]
+        public IEnumerator TestSpawn()
+        {
+
+            yield return new WaitForSeconds(0.1f);
+
+            
+        }
+    }
