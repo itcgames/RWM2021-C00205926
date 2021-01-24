c1ba78a67b8a8f49e398441708beb23b25a71498
jakec <jakec@DESKTOP-JTC9HU6>
2021-01-24 21:52:33 +0000

    Bomb plants on click

---
+++ Assets/BombComponent/Scripts/BombTileMap/BombSpawnerTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class BombSpawnerTile : MonoBehaviour
+{
+    public Tilemap tm;
+    public GameObject bomb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseToWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3Int index = tm.WorldToCell(mouseToWorld);
+
+            Vector3 centreBomb = tm.GetCellCenterWorld(index);
+
+            Instantiate(bomb, centreBomb, Quaternion.identity);
+
+        }
+    }
+}
